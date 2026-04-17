d2ac1c4928b16badec50bbbba17cd332d2456718
rae <rae@raes-MacBook-Pro.local>
2026-04-17 11:33:26 -0500

    add GA4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-56CLED2HFD" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-56CLED2HFD');`,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
